63384b9e36d78e8298f3e30404987fa490d5ae11
Luis Antonio Ostrowski Jr <ostrowskijr@gmail.com>
2020-06-22 17:08:24 -0300

    Finalização operções de CRUD API.

--- index.js
+++ index.js
@@ -22,10 +22,47 @@ app.get("/estados", (req, res, next) => {
 
 app.post("/estados", (req, res, next) => {
     var json = req.body;
-    Estado.create(json).then(() =>{
-        res.redirect("/estados")
-    }).catch((error) => {
-        res.send("Erro ao inserir estado na Base de dados: " + error)
+    Estado.findOne({
+        raw: true,
+        where : {
+            "uf" : json.uf;
+        }
+    }).then((estado){
+        if (estado) {
+            res.send("Estado já cadastrado com a UF informada: " + json.uf)
+        } else {
+            Estado.create(json).then(() =>{
+                res.redirect("/estados")
+            }).catch((error) => {
+                res.status(500).send("Erro ao inserir estado na Base de dados: " + error)
+            })
+        }
+    })
+})
+
+app.put("/estados", (req, res, next) => {
+    var json = req.body;
+    Estado.update(json, {
+        where : {
+            "uf" : json.uf
+        }
+    }).then(result => {
+        res.send("{ status : Registro alterado com sucesso! }");
+    }).then(error => {
+        res.status(500).send("Ocorreu um erro ao realizar Update: " + error)
+    })
+})
+
+app.delete("/estados/:uf", (req, res, next) => {
+    var uf = req.params.uf;
+    Estado.destroy({
+        where : {
+            "uf" : json.uf
+        }
+    }).then(result => {
+        res.send("{status : Registro excluído com sucesso! }");
+    }).catch(error => {
+        res.status(500).send("Erro ao excluir o estado do banco de dados: " + error)
     })
 })
 
