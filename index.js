263ba1de4e23b7dd44fc6b4d52dd5021a00a27c9
Luis Antonio Ostrowski Jr <ostrowskijr@gmail.com>
2020-06-23 19:23:00 -0300

    Retorno mensagem em Json.

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Minha primeira API Rest em Node Js...");
+    //res.send("Minha primeira API Rest em Node Js...");
+    res.render("index");
 })
 
 app.get("/estados", (req, res, next) => {
@@ -16,7 +17,9 @@ app.get("/estados", (req, res, next) => {
     }).then((Estados) => {
         res.status(200).json(Estados);
     }).catch((error) => {
-        res.status(404).send("Erro ao acessar base de Dados Estados: " +  error);
+        res.status(404).json({
+            error : "Erro ao acessar base de Dados Estados: " +  error
+        });
     })
 })
 
@@ -30,16 +33,24 @@ app.post("/estados", (req, res, next) => {
         }
     }).then((estado) => {
         if (estado) {
-            res.status(404).send("Estado já cadastrado com a UF informada: " + json.uf)
+            res.status(404).json({
+                message : "Estado já cadastrado com a UF informada: " + json.uf
+            })
         } else {
             Estado.create(json).then(() =>{
-                res.redirect("/estados")
+                res.status(200).json({
+                    message : "Registro criado com sucesso!"
+                });
             }).catch((error) => {
-                res.status(500).send("Erro ao inserir estado na Base de dados: " + error);
+                res.status(500).json({
+                    message : "Erro ao inserir estado na Base de dados: " + error
+                });
             })
         }
     }).catch((error) => {
-        res.status(500).send("Erro ao consulta Estado: " + error);
+        res.status(500).json({
+            message : "Erro ao consulta Estado: " + error
+        });
     })
 })
 
@@ -47,16 +58,22 @@ app.put("/estados", (req, res, next) => {
     var json = req.body;
     var id = json.id;
     if (id === null || id === undefined){
-        res.status(404).send({"{error : Id obrigatório para alteração}");
+        res.status(404).json({
+            error : "Id obrigatório para alteração"
+        });
     } else {
         Estado.update(json, {
             where : {
                 "id" : id
             }
         }).then((result) => {
-            res.status(200).send("{ status : Registro alterado com sucesso! }");
+            res.status(200).json({
+                message : "Registro alterado com sucesso!"
+            });
         }).then(error => {
-            res.status(500).send("Ocorreu um erro ao realizar Update: " + error);
+            res.status(500).json({
+                error : "Ocorreu um erro ao realizar Update: " + error
+            });
         })
     }
 })
@@ -64,16 +81,22 @@ app.put("/estados", (req, res, next) => {
 app.delete("/estados", (req, res, next) => {
     var id = req.body.id;
     if (id === null || id === undefined){
-        res.status(404).send({"{error : Id obrigatório para alteração}");
+        res.status(404).json({
+            error : "Id obrigatório para alteração"
+        });
     } else {
         Estado.destroy({
             where : {
                 "id" : id
             }
         }).then((result) => {
-            res.status(200).send("{status : Registro excluído com sucesso! }");
+            res.status(200).json({
+                message : "Registro excluído com sucesso!"
+            });
         }).catch(error => {
-            res.status(500).send("Erro ao excluir o estado do banco de dados: " + error);
+            res.status(500).json({
+                error : "Erro ao excluir o estado do banco de dados: " + error
+            });
         })
     }
 })
